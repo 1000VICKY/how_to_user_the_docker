d101c1cd503934d0c0d9e877560f41d78a2b5cb8
senbiki akifumi <aki.sen.1209@gmail.com>
2018-08-26 19:23:43 +0900

    (1)docker 基本的な開発環境の検証環境

---
+++ source-file/index.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once("./configure.php");
+phpinfo();
+try {
+
+    /* リクエストから得たスーパーグローバル変数をチェックするなどの処理 */
+
+    // データベースに接続
+    $pdo = new PDO(
+        'mysql:dbname=exam_system;host=mysql;charset=utf8mb4',
+        'root',
+        'root_password',
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+    var_dump($pdo);
+    /* データベースから値を取ってきたり， データを挿入したりする処理 */
+
+    foreach($pdo->query('show databases') as $row) {
+        print_r($row);
+    }
+    $res = $pdo->query("select * from exam_system.image_table");
+    foreach($res->fetch() as $key => $value) {
+        var_dump("<img src=\"data:image/jpeg;base64,".$value."\">");
+    }
+} catch (PDOException $e) {
+
+    // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
+    // - もし手抜きしたくない場合は普通にHTMLの表示を継続する
+    // - ここではエラー内容を表示しているが， 実際の商用環境ではログファイルに記録して， Webブラウザには出さないほうが望ましい
+    header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    exit($e->getMessage());
+
+}
+
+// Webブラウザにこれから表示するものがUTF-8で書かれたHTMLであることを伝える
+// (これか <meta charset="utf-8"> の最低限どちらか1つがあればいい． 両方あっても良い．)
+header('Content-Type: text/html; charset=utf-8');
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Example</title>
+    </head>
+    <body>
+        <!-- ここではHTMLを書く以外のことは一切しない -->
+    </body>
+</html>
